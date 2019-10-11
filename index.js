9c8825b5290a53a8cdba34a640430cc7ff771b32
jdalida <jmdalida11@gmail.com>
2019-10-11 00:35:16 +0000

    for heroku

--- index.js
+++ index.js
@@ -21,10 +21,10 @@ app.get('/webhook', (req, res) => {
   let mode = req.query['hub.mode'];
   let token = req.query['hub.verify_token'];
   let challenge = req.query['hub.challenge'];
-    
-  if (mode && token) {
+   
+  if (token) {
   
-    if (mode === 'subscribe' && token === constant.VERIFY_TOKEN) {
+    if (token === constant.VERIFY_TOKEN) {
       
       console.log('WEBHOOK_VERIFIED');
       res.status(200).send(challenge);
